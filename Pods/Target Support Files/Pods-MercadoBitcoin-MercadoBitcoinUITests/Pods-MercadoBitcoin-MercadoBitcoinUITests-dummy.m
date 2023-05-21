311db90c061ed946b79dff3239d7fbf433d800e1
NJ Development <nj.development83@gmail.com>
2023-05-21 12:49:52 -0300

    Minor adjustment

--- Pods/Target Support Files/Pods-MercadoBitcoin-MercadoBitcoinUITests/Pods-MercadoBitcoin-MercadoBitcoinUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MercadoBitcoin_MercadoBitcoinUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MercadoBitcoin_MercadoBitcoinUITests
-@end
--- Pods/Target Support Files/Pods-MercadoBitcoin/Pods-MercadoBitcoin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MercadoBitcoin : NSObject
-@end
-@implementation PodsDummy_Pods_MercadoBitcoin
-@end
